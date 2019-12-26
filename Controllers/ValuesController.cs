df9dec73a3fa89ee0597a7e71a27962acf3d064e
liulida <1308919126@qq.com>
2019-12-26 16:50:04 +0800

    wwww

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace JenkinsDemo.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value3", "value4" };
+            return new string[] { "value3", "v5" };
         }
 
         // GET api/values/5
