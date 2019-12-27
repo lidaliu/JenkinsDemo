2dee60153b4ae05d26cd1c9ac570b77160df51a7
liulida <1308919126@qq.com>
2019-12-27 17:00:44 +0800

    ssss

--- setup.sh
+++ setup.sh
@@ -1,1 +1,23 @@
-﻿
+﻿#!/bin/bash -il
+echo 'start'
+
+# variables
+echo 'variables:'
+GITHASH=`git rev-parse --short HEAD`
+IMGNAME=netcore-demo
+CONTAINER=netcore-demo-web
+# publish
+echo 'publish:'
+rm -rf ./publish
+dotnet publish -o ./publish
+# image
+echo 'image:'
+docker build -t $IMGNAME:$GITHASH .
+docker tag $IMGNAME:$GITHASH $IMGNAME:latest
+docker rmi -f $(docker images -q -f dangling=true)
+# container
+echo 'container:'
+docker stop $CONTAINER || true && docker rm -f $CONTAINER || true
+docker run -d  -p 5005:5005 --name $CONTAINER $IMGNAME
+
+echo 'done!'
